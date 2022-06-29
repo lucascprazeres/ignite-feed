a56c216eb3810093ac4ec22c50d659a6480b49b8
Lucas Prazeres <olucascostadp@gmail.com>
2022-06-29 20:35:15 -0300

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from './components/Header'
+
 import './global.css'
 
 function App() {
   return (
-    <h1>Hello, world</h1>
+    <Header />
   )
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css'
+
+import igniteLogo from '../../assets/ignite-logo.svg'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img
+        src={igniteLogo}
+        alt="Dois triângulos verdes sobrepostos apontando para o canto superior direito"
+      />
+    </header>
+  )
+}
