6bcd5607e0103efdbe34a1a2602bcb670e357f68
Lucas Prazeres <olucascostadp@gmail.com>
2022-06-30 15:37:03 -0300

    feat: creates avatar component

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,10 +1,11 @@
 import { Trash, ThumbsUp } from 'phosphor-react'
+import { Avatar } from '../Avatar'
 import styles from './styles.module.css'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/lucascprazeres.png" />
+      <Avatar hasBorder={false} src="https://github.com/lucascprazeres.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from '../Avatar'
 
 import styles from './styles.module.css'
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/51201126?v=4" />
+        <Avatar src="https://avatars.githubusercontent.com/u/51201126?v=4" />
         
         <strong>Lucas Prazeres</strong>
         <span>Web developer</span>
