2ae9df60b97f8c8c0859bd4d160bfad777761455
Lucas Prazeres <olucascostadp@gmail.com>
2022-06-30 14:26:41 -0300

    feat: add commentForm component

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -31,6 +31,18 @@ export function Post({ author, content }) {
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Escreva um comentário...'
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
